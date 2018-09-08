4d78f566cec01e8a93d07b1c3003604e345aac36
yasas1 <32437531+yasas1@users.noreply.github.com>
2018-09-08 09:58:54 +0530

    updated

--- test/resources/views/users/create.blade.php
+++ test/resources/views/users/create.blade.php
@@ -2,59 +2,62 @@
 
 @section('content')
 
-<div>
+<div class="form-group">
 
-        <h1>Test User</h1> <br>
-            
-            @if(count($errors)>0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach($errors->all() as $error)
-                    <li> {{ $error}} </li>
-                    @endforeach
-                </ul>
-            </div> 
-            @endif
+    <h1>Test User</h1> <br>
+        
+        @if(count($errors)>0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                <li> {{ $error}} </li>
+                @endforeach
+            </ul>
+        </div> 
+        @endif
 
-            @if(\Session::has('success'))
-            <div class="alert alert-success">
-                <p> {{ \Session::get('success') }} </p>
+        @if(\Session::has('success'))
+        <div class="alert alert-success">
+            <p> {{ \Session::get('success') }} </p>
+        </div>
+        @endif 
+
+        <!-- "{{ url('users')}}" -->
+
+    {{-- <form method = "post" action = "/users" >
+        {{csrf_field()}}
+            <label class="lb" > UserName :</label><br>
+            <input class="form-control" type="text" name="username" placeholder="username"><br>
+    
+            <label class="lb" > Password :</label><br>
+            <input class="form-control" type="password" name="password" placeholder="password" ><br>
+
+            <label class="lb" > Email : </label><br>
+            <input class="form-control"  type="text" name="email" placeholder="email" > <br>
+                
+            <input type="submit" class="btn btn-primary" >
             </div>
-            @endif 
 
-            <!-- "{{ url('users')}}" -->
-    
-        {{-- <form method = "post" action = "/users" >
-            {{csrf_field()}}
-                <label class="lb" > UserName :</label><br>
-                <input class="form-control" type="text" name="username" placeholder="username"><br>
+        </form> --}}
         
-                <label class="lb" > Password :</label><br>
-                <input class="form-control" type="password" name="password" placeholder="password" ><br>
-    
-                <label class="lb" > Email : </label><br>
-                <input class="form-control"  type="text" name="email" placeholder="email" > <br>
-                       
-                <input type="submit" class="btn btn-primary" >
-                </div>
-    
-            </form> --}}
+        {!! Form::open(['method'=> 'POST','action'=>'UserController@store']) !!}
 
-            {!! Form::open() !!}
-                    <label class="lb" > UserName :</label><br>
-                    <input class="form-control" type="text" name="username" placeholder="username"><br>
+            <label  > UserName :</label>
+            {!! Form::text('username',null,['class'=>'form-control','placeholder'=>'username']) !!}
+    
+            <label > Password :</label><br>
+            {!! Form::password('password',['class'=>'form-control','placeholder'=>'password']) !!}
+ 
+            <label > Email : </label><br>
+            {!! Form::text('email',null,['class'=>'form-control','placeholder'=>'example@gmail.com']) !!}
             
-                    <label class="lb" > Password :</label><br>
-                    <input class="form-control" type="password" name="password" placeholder="password" ><br>
-        
-                    <label class="lb" > Email : </label><br>
-                    <input class="form-control"  type="text" name="email" placeholder="email" > <br>
-                           
-                    <input type="submit" class="btn btn-primary" >
-                    </div>
-        
-                </form>
+            <br>
+            {{Form::submit('SUBMIT', ['class'=>'btn btn-success pull-left'])}}
+            {{-- <input type="submit" class="btn btn-primary" > --}}
+            
     
-    </div>
+        {!! Form::close() !!}
     
-    @endsection
+</div>
+    
+@endsection
--- test/resources/views/users/edit.blade.php
+++ test/resources/views/users/edit.blade.php
@@ -2,55 +2,83 @@
 
 @section('content')
 
-<div>
+<div class="form-group">
 
-        <h1>Edit User</h1> <br>
-            
-            {{-- @if(count($errors)>0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach($errors->all() as $error)
-                    <li> {{ $error}} </li>
-                    @endforeach
-                </ul>
-            </div> 
-            @endif
-
-            @if(\Session::has('success'))
-            <div class="alert alert-success">
-                <p> {{ \Session::get('success') }} </p>
-            </div>
-            @endif  --}}
-
-            <!-- "{{ url('users')}}" -->
-    
-        <form method = "post" action = "/users/{{$user->id}}" >
-            {{csrf_field()}}
-
-            <input type="hidden" name="_method" value="PUT">
-
-                <label class="lb" > UserName :</label><br>
-                <input class="form-control" type="text" name="username" placeholder="username" value="{{$user->username}}"><br>
-                          
-                <label class="lb" > Email : </label><br>
-                <input class="form-control"  type="text" name="email" placeholder="email" value="{{$user->email}}"> <br>
-                       
-                <input type="submit" class="btn btn-info" value="UPDATE" >
-                
-    
-            </form> <br>
-
-            <form method = "post" action = "/users/{{$user->id}}" >
-                {{csrf_field()}}
-    
-                <input type="hidden" name="_method" value="DELETE">
-    
-                          
-                <input type="submit" class="btn btn-danger" value="DELETE" >
-                    
+    <h1>Edit User</h1> <br>
         
-            </form>
+        {{-- @if(count($errors)>0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                <li> {{ $error}} </li>
+                @endforeach
+            </ul>
+        </div> 
+        @endif
+
+        @if(\Session::has('success'))
+        <div class="alert alert-success">
+            <p> {{ \Session::get('success') }} </p>
+        </div>
+        @endif  --}}
+
+        <!-- "{{ url('users')}}" -->
+
+    {{-- <form method = "post" action = "/users/{{$user->id}}" >
+        {{csrf_field()}}
+
+        <input type="hidden" name="_method" value="PUT">
+
+            <label class="lb" > UserName :</label><br>
+            <input class="form-control" type="text" name="username" placeholder="username" value="{{$user->username}}"><br>
+                        
+            <label class="lb" > Email : </label><br>
+            <input class="form-control"  type="text" name="email" placeholder="email" value="{{$user->email}}"> <br>
+                    
+            <input type="submit" class="btn btn-info" value="UPDATE" >
+            
+
+    </form> <br> --}}
+
+    {!! Form::model($user,['method'=> 'PATCH','action'=>['UserController@update',$user->id]]) !!}
     
-    </div>
+    {{csrf_field()}}
+
+    <label  > UserName :</label>
+    {!! Form::text('username',null,['class'=>'form-control','placeholder'=>'username']) !!}
+
+
+    <label > Email : </label><br>
+    {!! Form::text('email',null,['class'=>'form-control','placeholder'=>'example@gmail.com']) !!}
     
-    @endsection
+    <br>
+    {{Form::submit('SUBMIT', ['class'=>'btn btn-info pull-left'])}}
+    {{-- <input type="submit" class="btn btn-primary" > --}}
+    
+
+    {!! Form::close() !!}
+    <br>
+
+    {{-- <form method = "post" action = "/users/{{$user->id}}" >
+
+        {{csrf_field()}}
+
+        <input type="hidden" name="_method" value="DELETE">
+
+                    
+        <input type="submit" class="btn btn-danger" value="DELETE" >
+            
+
+    </form> --}}
+
+    {!! Form::open(['method'=> 'DELETE','action'=>['UserController@destroy',$user->id]]) !!}
+    
+    {{csrf_field()}}
+
+    {{Form::submit('DELETE', ['class'=>'btn btn-danger pull-left'])}}
+
+    {!! Form::close() !!}
+    
+</div>
+    
+@endsection
